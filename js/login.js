b83fdecd92ff0bcfb341601ab1cf0843b2e55a71
Laisson Bruno <laissontkd@hotmail.com>
2023-02-07 17:59:07 -0300

    jogoMemoria

---
+++ js/login.js
@@ -0,0 +1,27 @@
+
+/* Codigo Desenvolvido por Laisson Bruno - Inspirado em videos e estudos pessoais */
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login_form');
+
+const validateInput = ({ target }) => {
+    if (target.value.length > 2) {
+      button.removeAttribute('disabled');
+      return;
+    }
+    
+    button.setAttribute('disabled', '');    
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player', input.value);
+    window.location = './pages/game.html'
+}
+
+
+
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
